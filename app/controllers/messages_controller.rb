50f33abdf46b9d0d3a719f228fbe7d223a3b4841
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2016-05-17 20:36:41 -0300

    Add New and Create action with view for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,21 @@ class MessagesController < ApplicationController
   before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
+    @messages = Message.all.order('created_at DESC')
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(messages_params)
+
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -18,9 +27,16 @@ class MessagesController < ApplicationController
   end
 
   def update
+    if @message.update(messages_params)
+      redirect_to @message
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
