68f8d89d82ea23e2b49c364561c162e9d30b8557
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2016-05-19 22:56:36 -0300

    Add Authenticate to Controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
-  before_action :find_message, only: [:create]
+  before_action :authenticate_user!
+  before_action :find_message, only: [:create, :edit, :update, :destroy]
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
   def create
     @comment = Comment.build_from(@message, current_user.id, comment_params[:body])
@@ -12,6 +14,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
+
   private
 
   def comment_params
@@ -20,6 +38,10 @@ class CommentsController < ApplicationController
 
   def find_message
     @message = Message.find(params[:message_id])
+  end
+
+  def find_comment
+    @comment = @message.comment_threads.find_by(id: params[:id], user: current_user.id)
   end
 
 end
